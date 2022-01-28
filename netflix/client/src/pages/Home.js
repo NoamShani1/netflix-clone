f7cf0b7cb809da1e9b1b0af2ee1b486d733a5237
Noam Shani <Noams156@gmail.com>
2022-01-28 17:56:52 +0100

    navbar ui added

--- netflix/client/src/pages/Home.js
+++ netflix/client/src/pages/Home.js
@@ -1,9 +1,9 @@
-import React from 'react';
 import './home.scss'
+import { Navbar } from '../components/Navbar/Navbar';
 
 const Home = () => {
-  return  <div className='home' >
-      'hello wrld'
+  return  <div className="home" >
+  <Navbar />
   </div>;
 };
 
