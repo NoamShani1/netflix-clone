b241bec548833f3b9f9df0f4d4258f9d8edb4733
Noam Shani <Noams156@gmail.com>
2022-01-29 20:47:56 +0100

    added navbar ui - scrolling ui with state

--- netflix/client/src/pages/Home.js
+++ netflix/client/src/pages/Home.js
@@ -2,8 +2,13 @@ import './home.scss'
 import { Navbar } from '../components/Navbar/Navbar';
 
 const Home = () => {
-  return  <div className="home" >
-  <Navbar />
+  return <div className="home" >
+    <Navbar />
+    <img
+     width='100%'
+      src="http://www.skarbakka.com/wp-content/uploads/2019/09/Blue_Tree.jpg"
+      alt=""
+    />
   </div>;
 };
 
