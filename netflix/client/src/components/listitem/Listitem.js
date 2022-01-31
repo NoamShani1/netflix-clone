f9da381d6dcfb4769536c3964dd7cb72bb4ecb2e
Noam Shani <Noams156@gmail.com>
2022-01-31 18:52:46 +0100

    added ui and automatic image fit to movie list

--- netflix/client/src/components/listitem/Listitem.js
+++ netflix/client/src/components/listitem/Listitem.js
@@ -4,7 +4,11 @@ import './listitem.scss'
 export default function Listitem() {
   return(
        <div className='listItem'> 
-       item
+       
+       <img
+        src="https://assets.whatsnewonnetflix.com/external_assets/sggkh+%5B%5Blxx*9*850*230_8_muochl_mvg%5Bzig%5B30490%5Bux4w0z237y1zw6w4591557910vw4vv06v5x30490.jpg"
+        alt=""
+      />
        </div>
   )
 }
