120999b46f43c251150ef80072e2d395e457641d
Noam Shani <Noams156@gmail.com>
2022-02-21 18:19:51 +0100

    added styles and video

--- netflix/client/src/App.js
+++ netflix/client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './app.scss'
 import Home from './pages/Home'
+import Watch from "./pages/watch/Watch";
 
 function App() {
   return (
     <div>
+    <Watch />
     <Home />
    </div>
   );
